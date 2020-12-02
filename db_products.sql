0d48ae0862369acecee680bb595952b9ba46de3f
Ignacio Ochoa <ignacio.ochoa.unicen@gmail.com>
2020-12-02 12:38:09 -0300

    se actualizo la base de datos

--- db_products.sql
+++ db_products.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-10-2020 a las 13:13:10
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.9
+-- Tiempo de generación: 02-12-2020 a las 16:36:02
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -39,8 +38,31 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`id_category`, `nombre`) VALUES
 (1, 'perifericos'),
-(2, 'impresoras'),
+(2, 'Impresoras'),
 (4, 'computadoras');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comments`
+--
+
+CREATE TABLE `comments` (
+  `id_comentario` int(255) NOT NULL,
+  `id_product` int(255) NOT NULL,
+  `comentario` varchar(255) NOT NULL,
+  `puntuacion` int(255) NOT NULL,
+  `id_user` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comments`
+--
+
+INSERT INTO `comments` (`id_comentario`, `id_product`, `comentario`, `puntuacion`, `id_user`) VALUES
+(1, 8, 'que lindo mouse', 4, 2),
+(2, 8, 'de los mejores mouses que haya probado, lo recomiendo totalmente.', 5, 2),
+(3, 7, 'ME encanta este teclado', 4, 2);
 
 -- --------------------------------------------------------
 
@@ -65,11 +87,12 @@ INSERT INTO `product` (`id_product`, `name`, `description`, `price`, `image_url`
 (4, 'reloj', 'grande', -10, 'https://www.cdrmedios.com/imgs/productos/productos37_23368.jpg?1928', 2),
 (5, 'gabinote', 'Compatible con\r\nSocket Intel\r\n  LGA20XX/LGA1366/LGA115X \r\n  High End Desktop i7', 1000, 'https://www.cdrmedios.com/imgs/productos/productos37_15424.jpg?7789', 4),
 (7, '\r\nTeclado Gamer Genius retroiluminado en español', 'Brillante luz de fondo (backlit) en varios colores.\r\nTeclado en español (Ñ)\r\nLas teclas del teclado en español para gaming Scorpion K215 tienen la mitad de altura que un teclado regular y duran hasta 2 millones de pulsaciones.', 1000, 'https://www.cdrmedios.com/imgs/productos/productos37_15086.jpg?30', 1),
-(8, 'Notebook Asus Dualcore ', 'Color gris (Star Grey).\r\nWindows 10 Home 64bit en Español pre instalado.\r\nProcesador Intel Celeron Dualcore N4000 1.1Ghz (hasta 2.6GHz con turbo boost).', 1000, 'https://www.cdrmedios.com/imgs/productos/productos37_23346.jpg?7156', 4),
+(8, 'Mouse gamer logitech', 'hermoso mouse para jugar', 99999, 'https://axa.com.ar/webaxa/9791-medium_default/mouse-gaming-optico-logitech-g-pro-rgb.jpg', 1),
 (10, '\r\nImpresora Multifunción Canon MG2510', 'Función de encendido automático.\r\nFunción de apagado automático.\r\nSoftware My Image Garden.', 1000, 'https://www.cdrmedios.com/imgs/productos/productos37_15355.jpg?3175', 2),
 (17, 'impresora', 'nueva', 1000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSomcIYCfgWSQkLGSeb-f-VJMO3uVAr_GS5MQ&usqp=CAU', 2),
 (18, 'speakers', 'truchos', 24, 'https://images.crutchfieldonline.com/ImageHandler/trim/620/378/products/2019/25/701/g701S801PMW-F.jpg', 1),
-(19, 'agregando', 'cosas', 4, 'https://images-na.ssl-images-amazon.com/images/I/41oI0bzIX8L._SX500_SY500_CR,0,0,500,500_.jpg', 1);
+(19, 'agregando', 'cosas', 4, 'https://images-na.ssl-images-amazon.com/images/I/41oI0bzIX8L._SX500_SY500_CR,0,0,500,500_.jpg', 1),
+(23, 'fdfd', 'fdf', 1, 'https://cnnespanol.cnn.com/wp-content/uploads/2017/06/170628112455-jaguar-xe-sv-780x439.jpg?quality=90&strip=all', 1);
 
 -- --------------------------------------------------------
 
@@ -80,16 +103,22 @@ INSERT INTO `product` (`id_product`, `name`, `description`, `price`, `image_url`
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `pass` varchar(255) DEFAULT NULL
+  `pass` varchar(255) DEFAULT NULL,
+  `img_url` varchar(255) NOT NULL,
+  `bio` varchar(255) NOT NULL,
+  `fecha_registro` date DEFAULT NULL,
+  `is_admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id_user`, `email`, `pass`) VALUES
-(1, 'maki', '$2y$10$T2DoFBVoKZLX7e7AkZ9ZOuv32T0Qh5mNl5hSZY6ESYcSjLcW9W4xm'),
-(2, 'igna', '$2y$10$3ICKF508Ct4sSk.YSy3xwuW6bos7Q99C2gTTxgTV2ldDwXcqCNmbe');
+INSERT INTO `users` (`id_user`, `email`, `pass`, `img_url`, `bio`, `fecha_registro`, `is_admin`) VALUES
+(1, 'maki', '$2y$10$T2DoFBVoKZLX7e7AkZ9ZOuv32T0Qh5mNl5hSZY6ESYcSjLcW9W4xm', 'https://media.discordapp.net/attachments/575273032512438272/783289158369280010/no_definido.jpg?width=415&height=475', '', '2020-09-24', 1),
+(2, 'igna', '$2y$10$3ICKF508Ct4sSk.YSy3xwuW6bos7Q99C2gTTxgTV2ldDwXcqCNmbe', 'https://cdn.discordapp.com/attachments/575273032512438272/783289175013064734/perfil.jpg', '', '2020-09-24', 1),
+(18, '1234', '$2y$10$2sxVjfoe03LZe8EfOC/QO.xiExqoaMACXasoZ9mRJj0uKcJ/SvwUK', 'https://media.discordapp.net/attachments/575273032512438272/783289158369280010/no_definido.jpg?width=415&height=475', '', '2020-12-01', 0),
+(19, '12345', '$2y$10$3hNhoqtdFqQqdS4gkCnb3OU0IYx5/TxviTyqf76SURMtwK.lUwqL.', 'https://media.discordapp.net/attachments/575273032512438272/783289158369280010/no_definido.jpg?width=415&height=475', '', '2020-12-02', 0);
 
 --
 -- Índices para tablas volcadas
@@ -100,6 +129,14 @@ INSERT INTO `users` (`id_user`, `email`, `pass`) VALUES
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id_category`);
+
+--
+-- Indices de la tabla `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `fk_comment_product` (`id_product`),
+  ADD KEY `fk_comment_idUser` (`id_user`);
 
 --
 -- Indices de la tabla `product`
@@ -122,23 +159,36 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id_comentario` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
 
 --
 -- Filtros para la tabla `product`
